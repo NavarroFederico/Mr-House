f76a356818be41722e4369daae09b8d9f3ef7a02
Ramiro <ramaaybar@gmail.com>
2023-03-06 19:03:03 -0300

    Realizando cambios de PortalControlador.java

--- src/main/java/MrHouse/controladores/PortalControlador.java
+++ src/main/java/MrHouse/controladores/PortalControlador.java
@@ -21,60 +21,79 @@ import org.springframework.web.multipart.MultipartFile;
  *
  * @author facuq
  */
-
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
-    
+
     @Autowired
     private ClienteServicios clienteServicios;
-    
+
     @GetMapping("/")
     public String index() {
         return "index.html";
     }
-    
+
     @GetMapping("/registrar")
     public String registrar() {
         return "registro.html";
     }
-    
+
+    @GetMapping("/registrarc")
+    public String registrarc() {
+        return "registroc.html";
+    }
+
+    @GetMapping("/registrari")
+    public String registrari() {
+        return "registroi.html";
+    }
+
     @PostMapping("/registro")
-    public String registro(@RequestParam String nombre , @RequestParam String email , @RequestParam String password ,
-            @RequestParam String password2 , ModelMap modelo, MultipartFile archivo) {
-            try {
+    public String registro(@RequestParam String nombre, @RequestParam String email, @RequestParam String password,
+            @RequestParam String password2, ModelMap modelo, MultipartFile archivo) {
+        try {
             clienteServicios.registrar(archivo, nombre, email, password, password2);
             modelo.put("exito", "Usuario registrado correctamente");
             return "index.html";
         } catch (MyException ex) {
             modelo.put("error", ex.getMessage());
-            modelo.put("nombre",nombre);
-            modelo.put("email",email);
+            modelo.put("nombre", nombre);
+            modelo.put("email", email);
             return "registro.html";
         }
     }
-    
+
     @GetMapping("/ingresar")
-    public String login(@RequestParam(required = false)String error , ModelMap modelo) {
+    public String login(@RequestParam(required = false) String error, ModelMap modelo) {
         if (error != null) {
             modelo.put("error", "Usuario o contraseña invalidas");
         }
         return "ingreso.html";
     }
-    
+
     @PreAuthorize("hasAnyRole('ROLE_INQUILINO','ROLE_ADMIN','ROLE_PROPIETARIO','ROLE_INMOBILIARIA')")
     @GetMapping("/inicio")
     public String inicio() {
         return "inicio.html";
     }
-    
+
     @GetMapping("/tabla")
     public String tabla() {
         return "tabla.html";
     }
-    
+
     @GetMapping("/contacto")
     public String contacto() {
         return "contacto.html";
+    }
+
+    @GetMapping("/propiedades")
+    public String propiedades() {
+        return "propiedades.html";
+    }
+
+    @GetMapping("/inmobiliarias")
+    public String inmobiliarias() {
+        return "inmobiliarias.html";
     }
 }
